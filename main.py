0c6c8cc0d44710d5d1cfa84f2034faeab284a0c8
Zyad Islam <zyadislam201@gmail.com>
2023-07-12 12:41:48 +0300

    Open Sheet with button

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ class MainApp(ctk.CTk):
 
         self.geometry("1000x600")
 
-        self.open_button = ctk.CTkButton(self, command=self.open_excel)
+        self.open_button = ctk.CTkButton(self, command=self.open_excel, text="Open excel")
+        self.open_button.place(relx=0.5, rely=0.8, anchor="center")
         self.excel = Excel(self)
 
+        
         # self.bind("<FocusOut>", exit)
-        self.open_excel()
+        
         self.mainloop()
 
     def open_excel(self):
@@ -30,8 +32,6 @@ class MainApp(ctk.CTk):
                     value = ws[pos].value
                     print(row, column)
                     self.excel.set(row, column=column, value=value)
-
-                    
 
 
 class Excel(ttk.Treeview):
@@ -48,7 +48,6 @@ class Excel(ttk.Treeview):
             self.heading(col, text=get_column_letter(col))
             self.insert("", index="end", iid=col)
             self.set(col, 0, col)
-
             
         self.bind("<MouseWheel>", lambda _: "break")
         self.place(x=0, y=0, relwidth=1, relheight=0.7)
